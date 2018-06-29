6405d8d76ffc4be3081b39ad52a149bf67cd811f
Lucas <lpferriani@gmail.com>
2018-06-29 04:47:23 -0300

    Alteracoes para funcionar no server

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var app = configura();
 
 var rotasProdutos = require('./app/routes/produtos')(app);
 
+var porta = process.env.PORT || 3000
 app.listen(3000, function() {
 	console.log('Servidor rodando...');
 });
