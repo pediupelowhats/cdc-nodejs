9c3a3500dc67ca002dd185b5a51046b5e15be903
Lucas <lpferriani@gmail.com>
2018-06-29 05:56:48 -0300

    atualizando

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -1,18 +1,19 @@
 module.exports = function(app){
 	app.get('/produtos', function(request, response){
-		response.send('<h1>DEU CERTO!!! HAHAHA!!!!</h1>');
 		// var mySql = require('mysql');
 		// var connection = mySql.createConnection({
-		// 	host: 'localhost',
-		// 	user: 'root',
-		// 	password: '',
-		// 	database: 'casadocodigo'
-		// });;
+		// 	host: 'mysql.hostinger.com.br',
+		// 	user: 'u674129031_cdc',
+		// 	password: 'Euvoda1s32',
+		// 	database: 'u674129031_cdc'
+		// });
+		var porta = process.env.PORT || 3000;
+		response.send('<h1>Aplicação rodando no heroku na porta ' + porta + '</h1>');
 
 		// connection.query('select * from livros', function(err,results){
 		// 	response.send(results);
 		// });
 
 		// connection.end();
-	});
+		});
 }
