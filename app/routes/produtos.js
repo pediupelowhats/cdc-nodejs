89611423121be4e8677037c2a45af61fbf320945
Lucas <lpferriani@gmail.com>
2018-06-29 03:56:28 -0300

    Alteracao para teste sem BD

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -1,17 +1,18 @@
 module.exports = function(app){
 	app.get('/produtos', function(request, response){
-		var mySql = require('mysql');
-		var connection = mySql.createConnection({
-			host: 'localhost',
-			user: 'root',
-			password: '',
-			database: 'casadocodigo'
-		});;
+		response.send('<h1>DEU CERTO!!! HAHAHA!!!!</h1>');
+		// var mySql = require('mysql');
+		// var connection = mySql.createConnection({
+		// 	host: 'localhost',
+		// 	user: 'root',
+		// 	password: '',
+		// 	database: 'casadocodigo'
+		// });;
 
-		connection.query('select * from livros', function(err,results){
-			response.send(results);
-		});
+		// connection.query('select * from livros', function(err,results){
+		// 	response.send(results);
+		// });
 
-		connection.end();
+		// connection.end();
 	});
 }
